9e6925bd136deb4f3536445cf396f4ae131583d8
faritg1 <juanagelves@gmai.com>
2023-08-27 13:34:13 -0500

    feat:💄: Diseñando y agregando contenido en la main

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
-let menuIcon = document.querySelector("#menu")
-let sidebar = document.querySelector("#sidebar")
+let menuIcon = document.querySelector("#menu");
+let sidebar = document.querySelector("#sidebar");
+let container = document.querySelector("#container");
 
 /* Evento para reducir la informacion en el sidebar */
 menuIcon.onclick = () => {
     sidebar.classList.toggle("small-sidebar")
+    container.classList.toggle("large-container")
 } 
 
