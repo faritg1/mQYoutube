4af6d187f714c044c3a0161f86d41a24e6dc34b8
faritg1 <juanagelves@gmai.com>
2023-08-26 21:41:41 -0500

    feat:💄: Diseño interaccion del sidebar menu

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-console.log("FUNCIONA");
+let menuIcon = document.querySelector("#menu")
+let sidebar = document.querySelector("#sidebar")
+
+/* Evento para reducir la informacion en el sidebar */
+menuIcon.onclick = () => {
+    sidebar.classList.toggle("small-sidebar")
+} 
+
