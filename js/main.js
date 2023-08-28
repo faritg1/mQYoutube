03726d4c0c3c6bf74fb879bdd9f90d46c1542906
faritg1 <juanagelves@gmai.com>
2023-08-28 00:04:07 -0500

    feat:✨: Agregando api de prueba
    Añadiento api para extraer algunas infomaciones y probando que funcione

--- js/main.js
+++ js/main.js
@@ -12,6 +12,7 @@ menuIcon.onclick = () => {
 @creativecode787 */
 
 const url = 'https://youtube138.p.rapidapi.com/channel/details/?id=UC8fkwsjcI_MhralEX1g4OBw&hl=en&gl=US';
+const video = 'https://youtube138.p.rapidapi.com/channel/videos/?id=UC8fkwsjcI_MhralEX1g4OBw&hl=en&gl=US';
 const options = {
 	method: 'GET',
 	headers: {
@@ -20,15 +21,34 @@ const options = {
 	}
 };
 
-let fnYoutube = async() => {
-    try {
-        let peticion = await fetch(url, options);
-        let res = await peticion.json();
-        console.log(res);
-    } catch (error) {
-        console.error(error);
-    }   
+let fnChannel = async(url,options) => {
+    let peticion = await fetch(url, options);
+    let res = await peticion.json();
+    let profile = document.querySelector("#profile");
+    profile.innerHTML = null;
+    profile.insertAdjacentHTML("beforeend", /* html */`
+    <div id="profile" class="d-flex">
+    <img src="${res.avatar[0].url}" alt="">
+        <div class="vid-info">
+            <a href="./video/video.html">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non, velit!</a>
+            <p>Lorem, ipsum.</p>
+            <p>15k Views &bull; 2 days</p>
+        </div>
+    </div>
+    `)
 }
+fnChannel(url, options);
+
+let fnVideo = async(url, options) => {
+    let peticion = await fetch(url, options);
+    let res = await peticion.json();
+    let video = document.querySelector("#vid-list");
+    video.innerHTML = null;
+    video.insertAdjacentHTML("beforeend", /* html */ `
+        <img class="thumbnail" src=" ${res.contents[0].video.movingThumbnails[0].url} " alt="">
+    `)
+}
+fnVideo(video,options);
 
 
 
