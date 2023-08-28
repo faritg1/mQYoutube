756b1346b179e06463c38c32d9a4f08c9bd782f0
faritg1 <juanagelves@gmai.com>
2023-08-27 22:35:57 -0500

    feat:✨: Agregando api
    Añadiendo api de prueba

--- js/main.js
+++ js/main.js
@@ -8,3 +8,27 @@ menuIcon.onclick = () => {
     container.classList.toggle("large-container")
 } 
 
+/* COMENZANDO API  UC8fkwsjcI_MhralEX1g4OBw
+@creativecode787 */
+
+const url = 'https://youtube138.p.rapidapi.com/channel/details/?id=UC8fkwsjcI_MhralEX1g4OBw&hl=en&gl=US';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'a402962d30msha03577b2e46b364p19c5cejsn28de757b6c50',
+		'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
+	}
+};
+
+let fnYoutube = async() => {
+    try {
+        let peticion = await fetch(url, options);
+        let res = await peticion.json();
+        console.log(res);
+    } catch (error) {
+        console.error(error);
+    }   
+}
+
+
+
