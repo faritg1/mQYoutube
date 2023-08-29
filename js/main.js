d9056ba5f74267070e1ac2c0ccd019de9a77e571
faritg1 <juanagelves@gmai.com>
2023-08-28 19:12:41 -0500

    feat:♻️: Organizando js y json sidebar search

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import { searchAll } from "./get";
+import { searchAll } from "./get.js";
 let menuIcon = document.querySelector("#menu");
 let sidebar = document.querySelector("#sidebar");
 let container = document.querySelector("#container");
@@ -9,46 +9,7 @@ menuIcon.onclick = () => {
     container.classList.toggle("large-container")
 } 
 
-/* COMENZANDO API  UC8fkwsjcI_MhralEX1g4OBw
-@creativecode787 */
-
-const url = 'https://youtube138.p.rapidapi.com/channel/details/?id=UC8fkwsjcI_MhralEX1g4OBw&hl=en&gl=US';
-const video = 'https://youtube138.p.rapidapi.com/channel/videos/?id=UC8fkwsjcI_MhralEX1g4OBw&hl=en&gl=US';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'a402962d30msha03577b2e46b364p19c5cejsn28de757b6c50',
-		'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
-	}
-};
-
-let fnChannel = async(url,options) => {
-    let peticion = await fetch(url, options);
-    let res = await peticion.json();
-    let profile = document.querySelector("#profile");
-    profile.innerHTML = null;
-    profile.insertAdjacentHTML("beforeend", /* html */`
-    <div id="profile" class="d-flex">
-    <img src="${res.avatar[0].url}" alt="">
-        <div class="vid-info">
-            <a href="./video/video.html">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non, velit!</a>
-            <p>Lorem, ipsum.</p>
-            <p>15k Views &bull; 2 days</p>
-        </div>
-    </div>
-    `)
-}
-fnChannel(url, options);
-
-let fnVideo = async(url, options) => {
-    let peticion = await fetch(url, options);
-    let res = await peticion.json();
-    let video = document.querySelector("#vid-list");
-    video.innerHTML = null;
-    video.insertAdjacentHTML("beforeend", /* html */ `
-        <img class="thumbnail" src=" ${res.contents[0].video.movingThumbnails[0].url} " alt=""> 
-    `)}
-fnVideo(video,options);
+/* COMENZANDO API  UC8fkwsjcI_MhralEX1g4OBw @creativecode787 */
 
 /* ------------------- IMPORT SearchAll ---------------------- */
 document.querySelector("#charSearch").addEventListener("change", (e) =>{
