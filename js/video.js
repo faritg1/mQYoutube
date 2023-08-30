34d4af7a36cdd0175a959623787c0b8fc71d5d93
faritg1 <juanagelves@gmai.com>
2023-08-29 20:48:43 -0500

    feat:💄: Organizando estilos sidebar search

--- js/video.js
+++ js/video.js
@@ -1,6 +1,8 @@
 import { rightSidebar } from "./sidebarPlayVideo.js";
 import { fnIframe } from "./sidebarPlayVideo.js";
+import { searchAll } from "./search.js";
 
+searchAll();
 let storageId = localStorage.getItem("ID");
 //let storageDate = localStorage.getItem("DATE");
 rightSidebar();
